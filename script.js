3cfadf591ec4c3d0c789fe9648e96a0223b6df97
hikko624 <hikko624@gmail.com>
2021-05-29 22:24:31 +0900

    見れないようにした、クリックで見れるようにした

--- script.js
+++ script.js
@@ -14,14 +14,44 @@ inputCsv.addEventListener('change', function (e) {
             console.log(output);
         };
         reader.readAsText(files[0]);
-
     } else {
         // ファイルが受け取れなかった際の処理
+        alert('Can not load file.');
     }
 }, false);
 
-function createResult(result) {
+// クリックすると曲名と投稿者が表示される処理
+function clickToShowMusic(e) {
+    var buttonElement = document.getElementById(e.target.id);
+    buttonElement.textContent = '投稿者を見る';
+    buttonElement.onclick = clickToShowName
+    var targetTableRow = buttonElement.parentElement.parentElement;
+    targetTableRow.childNodes[0].className = '';
+}
 
+// クリックすると曲名と投稿者が表示される処理
+function clickToShowName(e) {
+    var buttonElement = document.getElementById(e.target.id);
+    buttonElement.textContent = 'この曲に決定する';
+    buttonElement.onclick = clickToSubmit;
+    var targetTableRow = buttonElement.parentElement.parentElement;
+    targetTableRow.childNodes[1].className = '';
+}
+
+// スコアタ曲を決定する
+function clickToSubmit(e) {
+    var buttonElement = document.getElementById(e.target.id);
+    var result = confirm('本当にこの曲にしますか？');
+    if (result) {
+        var targetTableRow = buttonElement.parentElement.parentElement;
+        targetTableRow.className = 'music-submit';
+        buttonElement.onclick = '';
+        buttonElement.textContent = '確定しました';
+    }
+}
+
+// csvファイルを読み込んだらtableを作成する
+function createResult(result) {
     let postInfoArray = result.split("\n");
     // 各投稿者の投稿情報のリスト[timestamp, CN, musicName...]
     let tableHeadList = postInfoArray[0].split(',');
@@ -38,14 +68,17 @@ function createResult(result) {
         cellHeadMusicName.appendChild(document.createTextNode("曲名"));
         var cellHeadPostName = document.createElement("td");
         cellHeadPostName.appendChild(document.createTextNode("投稿者"));
+        var cellHeadButton = document.createElement("td");
         rowHead.appendChild(cellHeadMusicName);
         rowHead.appendChild(cellHeadPostName);
+        rowHead.appendChild(cellHeadButton);
         tblHead.appendChild(rowHead);
 
         var tblBody = document.createElement('tbody');
         for (let j = 1; j < postInfoArray.length; j++) {
             var rowBody = document.createElement('tr');
             var postInfo = postInfoArray[j].split(',');
+            var id = 'music' + i + 'post' + j;
             // 投稿していない場合、スキップ
             if (!postInfo[i].trim()) {
                 continue;
@@ -53,11 +86,21 @@ function createResult(result) {
 
             var cellBodyMusicName = document.createElement('td');
             cellBodyMusicName.appendChild(document.createTextNode(postInfo[i]));
-            rowBody.appendChild(cellBodyMusicName);
+            cellBodyMusicName.className = 'text-blur';
             var cellBodyPostName = document.createElement('td');
             var postName = postInfo[1];
             cellBodyPostName.appendChild(document.createTextNode(postName));
+            cellBodyPostName.className = 'text-blur';
+            var cellBodyButton = document.createElement('td');
+            var showNameButton = document.createElement('button');
+            showNameButton.textContent = 'この曲を見る';
+            showNameButton.className = 'button';
+            showNameButton.onclick = clickToShowMusic;
+            showNameButton.id = id;
+            cellBodyButton.appendChild(showNameButton);
+            rowBody.appendChild(cellBodyMusicName);
             rowBody.appendChild(cellBodyPostName);
+            rowBody.appendChild(cellBodyButton);
             tblBody.appendChild(rowBody);
         }
         tbl.appendChild(tblHead);
@@ -69,6 +112,5 @@ function createResult(result) {
         element.appendChild(tbl);
         response.appendChild(element);
     }
-
     return response;
 }
